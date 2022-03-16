810bd9ac98de03ad68d2560c407ca1b8dae135a0
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2022-03-17 03:10:00 +0530

    Create 51-intersection-of-linked-list.java

---
+++ 51-intersection-of-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+import java.util.HashSet;
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> nodes = new HashSet<>();
+        while(headA != null) {
+            nodes.add(headA);
+            headA = headA.next;
+        }
+        while(headB != null) {
+            if(nodes.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
